e49fec22f2a39bb2cd210dec2804f4e1014ac216
AymanMagdy <magdia883@icloud.com>
2019-04-06 19:45:44 +0200

    File edited

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,2 +1,19 @@
-course.sh
-course.sh
+# /bin/bash
+# This is the code of the needed task;
+y="$(ls -1 | wc -l)" 
+echo "Enter the number of files : "
+read x
+
+if [[ $x == $y ]]
+then echo "Well done!"
+exit
+else if [[ $x > $y ]]
+then 
+echo "The number is too high."
+trap "$0" 0
+else if [[ $x < $y ]]
+then echo "The number is too low."
+trap "$0" 0
+fi
+fi
+fi
