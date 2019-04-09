89ec7e19b38530f163120e2edc657e549c20bb92
AymanMagdy <magdia883@icloud.com>
2019-04-09 13:08:24 +0200

    Editing the file

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,19 +1,27 @@
 # /bin/bash
 # This is the code of the needed task;
-y="$(ls -1 | wc -l)" 
-echo "Enter the number of files : "
-read x
+function guess(){
+    y="$(ls -1 | wc -l)"
 
-if [[ $x == $y ]]
-then echo "Well done!"
-exit
-else if [[ $x > $y ]]
-then 
-echo "The number is too high."
-trap "$0" 0
-else if [[ $x < $y ]]
-then echo "The number is too low."
-trap "$0" 0
-fi
-fi
-fi
+    while true;
+    do
+        echo "pleas enter your guess"
+        read  number
+        if [ $number -lt $y ]
+        then
+            echo "Your guess was too low."
+        elif [ $number -gt $y ]
+        then
+            echo "Your guess was too high."
+        else
+            echo "Thank you! Your guess was right!"
+        break;
+        fi
+    done
+}
+echo "Guess how many files in the current directory?"
+guess
+
+
+
+
